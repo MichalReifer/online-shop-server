d3f2b5e78d6edf3c07ab23b1ebe3c4c64069eae3
michal <michal.reifer@gmail.com>
2022-08-30 18:21:39 +0300

    Install Dotenv Package and Pass Variables To .env File
    also change running scripts in package.json: add dev script and change start back to normal

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const cors = require('cors');
 const cakeRoutes = require('./routes/cakeRoutes')
 const userRoutes = require('./routes/userRoutes')
 require('./editCollections/editCakes')
-
+require('dotenv').config()
 
 const app = express()
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = 8081
-mongoose.connect('mongodb://localhost:27017/cake_shop', { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(result => app.listen(PORT))
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result => app.listen(process.env.PORT))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-    res.send('Hello World')})
-  
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
+})
 app.use('/cakes', cakeRoutes)
 app.use('/users', userRoutes)
